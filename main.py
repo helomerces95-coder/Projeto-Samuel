b9598ae8e9f47efaadcdf2f360d67fc4fec07ebe
Guizinhounder07exc <guimenezes2507@gmail.com>
2025-11-21 17:05:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask #Importanto o Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #Chamando a classe Flask
 
-@app.route('/')
-def homepage():
-    return 'Flask'
+@app.route('/') #Aqui seria a rota, tipo a url do youtube
+def homepage(): 
+    return 'Flask' #Aqui é o aque vaia aparecer na tela (segundo o maluco do vídeo)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Aqui serve para verificar se o __name__ está na rodando na página main msm
     app.run(debug=True)   
